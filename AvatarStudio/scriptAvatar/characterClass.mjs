95faf75b51789a9091a3f7def5e1c2780d8fc43f
Rosalie <rosalie.schriewer@icloud.com>
2024-04-18 10:39:43 +0200

    undo/redo buttons no function yet but added

--- AvatarStudio/scriptAvatar/characterClass.mjs
+++ AvatarStudio/scriptAvatar/characterClass.mjs
@@ -6,12 +6,12 @@ import fs from 'fs';
 
 
 const bodyParts = {
-    iris: { name: 'eyes'},
-    hair: { name: 'hair_midlength' },
-    head: { name: 'skin' },
-    shirt: { name: 'shirt_base' },
-    pants: { name: 'pants_jogging' },
-    leg: null,
+    eye: { name: 'eyes', color: "#FFE7C0" },
+    hair: { name: 'hair_midlength', color: "#FFE7C0" },
+    eyebrow: { name: "eyebrow_hairier", color: "#8E3312" },
+    skin: { name: 'skin', color: "#FFE7C0" },
+    shirt: { name: 'shirt_base', color: "#FFE7C0" },
+    pants: { name: 'pants_jogging', color: "#FFE7C0" },
 }
 
 export class TCharacter extends THREE.Object3D {
@@ -24,18 +24,18 @@ export class TCharacter extends THREE.Object3D {
         const localSkinColor = localStorage.getItem("skincolor");
 
 
-        const categoryNames = ['shirt', 'hair', 'eyebrow', 'pants', 'cap', 'necklace', 'shoes', 'sunglasses', 'skirt', 'dress', 'halo', 'body', 'gloves', 'BezierCircle', 'Sphere', 'Plane'];
+        const categoryNames = ['shirt', 'hair', 'eyebrow', 'pants', 'cap', 'necklace', 'shoes', 'sunglasses', 'skirt', 'dress', 'halo', 'body', 'gloves', 'BezierCircle', 'Sphere', 'Plane', 'beard', 'backdrop'];
 
         loader.load("./AvatarStudio/mediaAvatar/character/avatar.gltf", (gltfModel) => {
             gltfModel.scene.position.set(scenePositions.x, scenePositions.y, scenePositions.z);
             this.add(gltfModel.scene);
 
-            function locateMeshColor(aBodyPart) {
+            function locateMeshToPhong(aBodyPart) {
                 const mesh = gltfModel.scene.children.find(child => child.name === aBodyPart)
-                if (mesh.name !== bodyParts.iris.name) {
-                    // Create a new MeshPhongMaterial
+                if (mesh.name !== bodyParts.eye.name) {
+
                     const phongMaterial = new THREE.MeshPhongMaterial();
-                    // Copy properties from MeshStandardMaterial to MeshPhongMaterial
+
                     phongMaterial.color.copy(mesh.material.color);
                     phongMaterial.map = mesh.material.map;
                     phongMaterial.normalMap = mesh.material.normalMap;
@@ -44,67 +44,40 @@ export class TCharacter extends THREE.Object3D {
                     // You would need to do similar for other properties like roughness, emissive, etc.
                     mesh.material = phongMaterial;
                 }
-
                 return mesh;
             }
-            console.log(gltfModel.scene);
 
+            console.log(gltfModel.scene);
             console.log(gltfModel.scene.parent.children);
 
-            const eyeMaterial = locateMeshColor(bodyParts.iris.name);
-            const hairMaterial = locateMeshColor(bodyParts.hair.name);
-            const skinMaterial = locateMeshColor(bodyParts.head.name);
-            const shirtMaterial = locateMeshColor(bodyParts.shirt.name);
-            const pantsMaterial = locateMeshColor(bodyParts.pants.name);
-
+            const eyeMesh = locateMeshToPhong(bodyParts.eye.name);
             const lights = gltfModel.scene.children.filter(child => child.isLight);
-
-            const eyebrows = gltfModel.scene.children.find(child => child.name === 'eyebrow')
-            if (eyebrows) {
-                gltfModel.scene.remove(eyebrows);
-            }
 
             lights.forEach(light => {
                 light.intensity = 40;
             });
 
-            this.setIrisColor = function (aColor) {
-                eyeMaterial.children[1].material.color.set(aColor);
-                eyeMaterial.children[1].material.transparent = true
+            function setChildColor(aColor) {
+                eyeMesh.children[1].material.color.set(aColor);
+                eyeMesh.children[1].material.transparent = true
             };
 
-            this.setHairColor = function (aColor) {
-                hairMaterial.material.color.set(aColor);
-            };
-
-            this.setSkinColor = function (aColor) {
-                skinMaterial.material.color.set(aColor);
-            };
-
-            this.setTopColor = function (aColor) {
-                shirtMaterial.material.color.set(aColor);
-            };
-
-            this.setBottomColor = function (aColor) {
-                pantsMaterial.material.color.set(aColor);
-            };
-
-            this.setColor = function () {
-                skinMaterial.material.color.set("#" + localSkinColor);
-                eyeMaterial.children[2].material.color.set("#" + localEyeColor);
-                hairMaterial.material.color.set("#" + localHairColor);
+            this.setColor = function (aMeshCategory, aColor) {
+                const locatedMesh = locateMeshToPhong(bodyParts[aMeshCategory].name);
+                bodyParts[aMeshCategory].color = aColor;
+                if (locatedMesh == eyeMesh) {
+                    setChildColor(aColor);
+                } else {
+                    locatedMesh.material.color.set(aColor);
+                }
             }
-            if ((localEyeColor && localHairColor && localSkinColor) !== null) {
-                this.setColor();
-            }
-
 
             function locateAllMeshes(scene) {
                 const meshCategories = {};
-            
+
                 categoryNames.forEach(categoryName => {
                     const processedMeshes = new Set(); // Keep track of processed meshes
-            
+
                     scene.children.forEach(child => {
                         if (child.name.startsWith(categoryName) && !processedMeshes.has(child)) {
                             const options = {};
@@ -122,26 +95,28 @@ export class TCharacter extends THREE.Object3D {
                         }
                     });
                 });
-            
-                startupMeshes(scene);
-            
+
+                setMesh(scene);
+
                 // Call the function to hide meshes initially            
                 // Save mesh categories to file
                 // saveMeshCategoriesToFile(meshCategories, 'meshCategories.json');
             }
 
-            function startupMeshes(scene) {
-                const startCategories = ['hair', 'shirt', 'pants'];
-            
+            function setMesh(scene) {
+                const startCategories = ['hair', 'shirt', 'pants', 'eyebrow'];
+
                 startCategories.forEach(category => {
                     const objectsInCategory = scene.children.filter(child => child.name.startsWith(category));
-            
+
                     // Find the child object with the name stored in bodyParts[category].name
                     const childWithName = objectsInCategory.find(child => child.name === bodyParts[category].name);
-            
+
                     if (childWithName) {
                         // If the child object is found, set its visibility to true
                         childWithName.visible = true;
+                        //this.setColor(category, bodyParts[category].color);
+
                     }
                 });
             }
@@ -154,14 +129,15 @@ export class TCharacter extends THREE.Object3D {
                 }
                 delete bodyParts[category].name //remove the old name 
                 bodyParts[category].name = name; //set the new name when old is removed
-                startupMeshes(gltfModel.scene);
+                setMesh(gltfModel.scene);
+                this.setColor(category, bodyParts[category].color);
             };
 
             function saveMeshCategoriesToFile(meshCategories, fileName) {
                 const jsonData = JSON.stringify(meshCategories, null, 2);
                 const blob = new Blob([jsonData], { type: 'application/json' });
                 const url = URL.createObjectURL(blob);
-            
+
                 const a = document.createElement('a');
                 a.href = url;
                 a.download = fileName;
@@ -173,6 +149,6 @@ export class TCharacter extends THREE.Object3D {
 
             locateAllMeshes(gltfModel.scene);
         });
-        
+
     }
 }
